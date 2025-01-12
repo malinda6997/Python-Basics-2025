d533b03c26ddfe22182b8d6fcb36113978e79392
Malinda Prabath <malindaprabath876@gmail.com>
2025-01-12 18:41:59 +0530

    add new files

---
+++ if_else.py
@@ -0,0 +1,31 @@
+# Example one
+# marks = 40
+
+# if marks < 0 or marks >100:
+#     print("Invalid marks")
+
+# elif marks >= 0 and marks < 35:
+#     print("You failed the exam")
+
+# elif marks >= 35 and marks < 55:
+#     print("You passed your grade is S ")
+
+# elif marks >= 55 and marks < 65:
+#     print("You passed your grade is C")
+
+# elif marks >= 65 and marks < 75:
+#     print("You passed your grade is B")
+
+# elif marks >= 75 and marks < 100:
+#     print("You passed your grade is A")
+
+
+
+# Example two
+marks = 65
+
+#ternary
+massage = "Pass" if marks > 35 else "Fail"
+
+print(massage)
+
---
+++ slicing.py
@@ -0,0 +1,16 @@
+# we can filter using slicing in list and tuple
+
+x = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+# get the first 5 elements
+y = x[:5]
+
+# get the last 5 elements
+z = x[5:]
+
+#get the all elements with out last element
+a = x[:-1]
+
+print(y)
+print(z)
+print(a)
